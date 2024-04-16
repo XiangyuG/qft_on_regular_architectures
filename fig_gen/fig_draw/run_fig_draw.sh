4318b4622651f6cd4d58e447bf3f3ad3a7244271
MinghoKwok <995666769@qq.com>
2024-04-16 10:16:42 -0400

    update run_fig_draw.sh

--- fig_gen/fig_draw/run_fig_draw.sh
+++ fig_gen/fig_draw/run_fig_draw.sh
@@ -18,17 +18,17 @@ PYTHON_SCRIPT="figure_draw.py"
 # Conditionally execute commands based on the architecture argument
 if [ "$ARCH" == "Sycamore" ]; then
     echo "Running script for Sycamore architecture..."
-    /opt/anaconda3/bin/python figure_draw.py Sycamore ../csv_data/our_sycamore_leq100.csv ../csv_data/sabre_sycamore_leq100.csv depth
-    /opt/anaconda3/bin/python figure_draw.py Sycamore ../csv_data/our_sycamore_leq100.csv ../csv_data/sabre_sycamore_leq100.csv swap
+    python3 figure_draw.py Sycamore ../csv_data/our_sycamore_leq100.csv ../csv_data/sabre_sycamore_leq100.csv depth
+    python3 figure_draw.py Sycamore ../csv_data/our_sycamore_leq100.csv ../csv_data/sabre_sycamore_leq100.csv swap
 elif [ "$ARCH" == "Heavy-hex" ]; then
     echo "Running script for Heavy-hex architecture..."
-    /opt/anaconda3/bin/python figure_draw.py Heavy-hex ../csv_data/our_heavy-hex.csv ../csv_data/sabre45_heavy-hex_100.csv depth
-    /opt/anaconda3/bin/python figure_draw.py Heavy-hex ../csv_data/our_heavy-hex.csv ../csv_data/sabre45_heavy-hex_100.csv swap
+    python3 figure_draw.py Heavy-hex ../csv_data/our_heavy-hex.csv ../csv_data/sabre45_heavy-hex_100.csv depth
+    python3 figure_draw.py Heavy-hex ../csv_data/our_heavy-hex.csv ../csv_data/sabre45_heavy-hex_100.csv swap
 elif [ "$ARCH" == "Lattice_Surgery" ]; then
-    /opt/anaconda3/bin/python figure_draw.py Lattice_Surgery ../csv_data/our_lattice_surgery_geq100.csv ../csv_data/sabre_lattice_surgery_geq100.csv depth
-    /opt/anaconda3/bin/python figure_draw.py Lattice_Surgery ../csv_data/our_lattice_surgery_geq100.csv ../csv_data/sabre_lattice_surgery_geq100.csv swap
+    python3 figure_draw.py Lattice_Surgery ../csv_data/our_lattice_surgery_geq100.csv ../csv_data/sabre_lattice_surgery_geq100.csv depth
+    python3 figure_draw.py Lattice_Surgery ../csv_data/our_lattice_surgery_geq100.csv ../csv_data/sabre_lattice_surgery_geq100.csv swap
 else
-    echo "Running script for a different architecture: $ARCH..."
+    echo "Illegal architecture input: $ARCH..., please input Sycamore / Heavy-hex / Lattice_Surgery"
 fi
 
 
